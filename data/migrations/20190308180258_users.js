cd1d1f79c2030e665580ec1896c2194647433b75
Scott Vojik <scott.vojik@gmail.com>
2019-03-09 09:12:42 -0800

    seeds and migrations ran

--- data/migrations/20190308180258_users.js
+++ data/migrations/20190308180258_users.js
@@ -5,8 +5,8 @@ exports.up = function(knex, Promise) {
     tbl.string('username', 24).notNullable().unique();
     tbl.string('password').notNullable();
     tbl.float('funds_balance');
-    tbl.integer('items_forSale').unsigned().references('itemId').inTable('items').defaultTo(null);
-    tbl.integer('wishlist').unsigned().references('itemId').inTable('items').defaultTo(null);
+    tbl.integer('items_for_sale').unsigned().references('itemId').inTable('items').defaultTo(null);
+    tbl.integer('wish_list').unsigned().references('itemId').inTable('items').defaultTo(null);
     tbl.integer('transactions').unsigned().references('itemId').inTable('items').defaultTo(null);
     tbl.string('img_url').notNullable();
   })
