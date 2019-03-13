97c3cace984826e7f5f889fc2659809c903caad9
Scott Vojik <scott.vojik@gmail.com>
2019-03-13 13:19:24 -0700

    fixing cors bug

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,9 +3,13 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const corsOptions = {
+  withCredentials: true,
+}
+
 module.exports = server => {
    server.use(helmet());
    server.use(express.json());
    server.use(morgan('short'));
-   server.use(cors());
+   server.use(cors(corsOptions));
 };
